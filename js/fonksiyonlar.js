1fc746f42c1c060677e46fb1044935117687d33e
AliGunlu <gunluali@gmail.com>
2024-06-02 20:36:19 +0300

    feat: Ses calma temek ozellikleri bitti

---
+++ js/fonksiyonlar.js
@@ -0,0 +1,42 @@
+import { modlar } from "./veri.js";
+
+const kokEtiket = document.querySelector("#kok-etiket")
+
+function UIGoster() {
+    modlar.forEach( (deger, index) => {
+        const yeniDiv = document.createElement("div")
+        yeniDiv.textContent = deger.tr
+
+        // mod görseli oluşturma
+        const yeniImg = document.createElement("img")
+        yeniImg.src = '../img/' + deger.mod + '.jpg'
+
+        // mod sesi oluşturma
+        const yeniAudio = document.createElement("audio")
+        yeniAudio.src = '../audio/' + deger.mod + '-sound.mp3'
+        yeniAudio.loop = true
+
+        yeniDiv.append(yeniImg)
+        yeniDiv.append(yeniAudio)
+
+        kokEtiket.append(yeniDiv)
+
+        //eventler
+        yeniDiv.addEventListener( 'click', ()=>{
+            if( yeniAudio.paused === true ) {
+                yeniAudio.play()
+                yeniDiv.classList.add("aktif")
+                yeniDiv.style.background = "gray"
+            } else {
+                yeniAudio.pause()
+                yeniDiv.classList.remove("aktif")
+                yeniDiv.style.background = "ivory"
+            }
+
+
+        } )
+    })
+}
+
+
+export {UIGoster}
